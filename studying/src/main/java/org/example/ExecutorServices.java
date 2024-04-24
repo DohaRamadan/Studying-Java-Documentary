639c3bb57512b0314baa4a8d6e6bc6a7a335c7d6
DohaRamadan <77820526+DohaRamadan@users.noreply.github.com>
2024-04-24 11:25:43 +0200

    documenting studying ExecutorService

---
+++ studying/src/main/java/org/example/ExecutorServices.java
@@ -0,0 +1,38 @@
+package org.example;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+public class ExecutorServices {
+
+    public void differentMethods(){
+        // create threads as needed and kill them as you go
+        ExecutorService ex = Executors.newCachedThreadPool();
+
+        // create single thread
+        ExecutorService ex2 = Executors.newSingleThreadExecutor();
+
+        // create a fixed number of threads
+        ExecutorService ex3 = Executors.newFixedThreadPool(10);
+    }
+
+    public void FutureInterface() throws InterruptedException, TimeoutException {
+        ExecutorService ex = Executors.newSingleThreadExecutor();
+        Future<String> f = ex.submit(() -> "Im a string");
+        try{
+            System.out.println(f.get(200, TimeUnit.MILLISECONDS));
+        }catch (InterruptedException | ExecutionException | TimeoutException e){
+            System.out.println("Method took longer than expected");
+        }
+        // we need to shut it down because it won't shut down on its own
+        ex.shutdown();
+    }
+
+
+
+}
--- studying/src/main/java/org/example/Main.java
+++ studying/src/main/java/org/example/Main.java
@@ -3,12 +3,18 @@ package org.example;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException, TimeoutException {
         // Comparing and Comparable
-        List<Employee> l = Arrays.asList(new Employee(2, "Mohamed"), new Employee(0, "Yassin"));
-        Comparator<Employee> comp = Comparator.comparing(Employee::getName);
-        l.sort(comp);
+//        List<Employee> l = Arrays.asList(new Employee(2, "Mohamed"), new Employee(0, "Yassin"));
+//        Comparator<Employee> comp = Comparator.comparing(Employee::getName);
+//        l.sort(comp);
+
+        ExecutorServices ex = new ExecutorServices();
+        ex.FutureInterface();
+
     }
 }
